db22323c8f722033e9b2d83ada661bca5a8c80c1
sa1n255 <samuellaurindodelima2004@gmail.com>
2022-11-27 00:44:38 -0300

    Rotas, rotas e mais rotas #2

--- src/server.js
+++ src/server.js
@@ -7,13 +7,15 @@ const stylePath = path.join(__dirname, 'public');
 const viewsPath = path.join(__dirname, 'views');
 
 // Import de Rotas
-const otherRoutes = require('./routes/routes');
-const loginRoutes = require('./routes/login');
+const agendaRoutes = require('./routes/agendaRoutes');
+const areamedicaRoutes = require('./routes/areamedicaRoutes');
+const cadastromedRoutes = require('./routes/cadastromedRoutes');
+const cadastroRoutes = require('./routes/cadastroRoutes');
+const loginRoutes = require('./routes/loginRoutes');
+const pesquisaRoutes = require('./routes/pesquisaRoutes');
 
 // Pegar o body da requisição agora é nativo do Express 😎
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Template Engine (Uso de CSS e Renderização de HTML)
@@ -27,10 +29,14 @@ const root = (req, res) => res.render('index', {});
 const noRoute = (req, res, next) => res.status(404).render('404');
 
 // Rotas
-app.get('/', root); // Rota Raiz
-app.use('/login', loginRoutes); // Rotas da tela de Login
-app.use(otherRoutes); // Todas as rotas
-app.use(noRoute); // Para rotas inexistentes
+app.get('/', root); 
+app.use('/agenda', agendaRoutes); 
+app.use('/area-medica', areamedicaRoutes);
+app.use('/cadastromed', cadastromedRoutes); 
+app.use('/cadastro', cadastroRoutes); 
+app.use('/login', loginRoutes); 
+app.use('/pesquisa', pesquisaRoutes); 
+app.use(noRoute); 
 
 // Escuta do Servidor Local
 app.listen(8080, () => console.log(`Servidor local ativo em http://localhost:8080`));
